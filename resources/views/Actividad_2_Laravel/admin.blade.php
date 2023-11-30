af1b0714a9fe1a67415343ea9a3696211d4a05bc
Marc <2023_marc.cuzcano@iticbcn.cat>
2023-11-30 18:08:56 +0100

    Mensaje Admin

---
+++ resources/views/Actividad_2_Laravel/admin.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+</head>
+
+<body>
+<!-- Texto de Bienvenida para el Administrador del centro-->
+<h3>"Benvingut administrador.El teu email és". {{$email}}</h3>
+</body>
+
+</html>
+
+
