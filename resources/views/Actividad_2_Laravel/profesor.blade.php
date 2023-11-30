ebb48d40f7c64e56fcaabd0f724b8de4e38b78a8
Marc <2023_marc.cuzcano@iticbcn.cat>
2023-11-30 18:08:27 +0100

    Mensaje Profesor

---
+++ resources/views/Actividad_2_Laravel/profesor.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+</head>
+
+<body>
+<!-- Texto de Bienvenida del Profesor-->
+<h3>"Benvingut professor. El teu email és " . {{$email}}</h3>
+</body>
+
+</html>
