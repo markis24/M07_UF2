69a8040549a4f46e42728fd3455152c20268e467
Marc <2023_marc.cuzcano@iticbcn.cat>
2023-11-30 18:13:00 +0100

    Archivo Web PHP

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\SignController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// los parametros van en el controlador
+// Escribimos el return
 // Ruta SingIn
 Route::get('/jostin/signin', [SignController::class, 'showSignIn']);
 
 // Ruta SignUp
 Route::get('/marc/signup', [SignController::class, 'showSignUp']);
+
+// Mostramos si hay un error
+Route::get('/error', function (){
+    return ("Error d'accés.");
+})->name('errorAcces.index');
+
+Route::post('/login', function () {
+        $email = request('email');
+        $password = request('password');
+});
+Route::get('/mostrar/{check}', function ($check){
+    return ("Has accedit correctament a aquesta ruta.");
+})->name('check');
+Route::post('/mostrar',function (Request $request){
+    $email = $request->email;
+    return "El meu email es " . $email  ;
+})
