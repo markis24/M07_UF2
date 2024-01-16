b0fd909a2cc6b1dc3e53981e9b55a6411eb10708
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-01-16 18:14:26 +0100

    Añadiendo redireccion a signin.index

--- my-project/app/Http/Controllers/SignController.php
+++ my-project/app/Http/Controllers/SignController.php
@@ -6,23 +6,23 @@ use Illuminate\Http\Request;
 
 class SignController extends Controller
 {
-    
+
     public function showSignIn()
     {
         return view('forms.signIn')->with('title', 'Iniciar sessió amb un usuari');
     }
-    
+
     public function showSignUp()
     {
         return view('forms.signUp')->with('title', "Creació d'un nou usuari");
-        
     }
-    public function SignIn(Request $request){
-        
-
+    public function SignIn(Request $request)
+    {
     }
 
-    public function SignUp(Request $request){
-            return redirect()->route('signin.index');
+    public function SignUp(Request $request)
+    {
+        // Después de la inserción, nos redirecciona a la ruta 'signin.index'
+        return ('<p>La inserció és correcte. <a href="' . route("signin.index") . '">Iniciar sessió usuari</a></p>');
     }
 }
