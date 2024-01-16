572593176539c0a5cf86107092767ffdb1320125
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-01-16 18:16:24 +0100

    Vista centre

--- my-project/resources/views/admin/centre.blade.php
+++ my-project/resources/views/admin/centre.blade.php
@@ -4,31 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Página de Administrador</title>
 </head>
-<body>
-    <h1>Benvingut administrador. El teu email és {{$email}}.</h1>
-    <h4>Llista de professors</h4>
+<body class="bg-light">
+    <h3>Llista de professors</h3>
+    <div class="container mt-5">
+        <br>
+        <br>
+        <table class="table">
+            <thead class="bg-info">
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">nom</th>
+                    <th scope="col">email</th>
+                </tr>
+            </thead>
+            <tbody class="table-primary">
+                @foreach($professors as $prof)
+                <tr>
+                    <td>{{ $prof['nom'] }}</td>
+                    <td>{{ $prof['email'] }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 
-    <table>
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>nom</th>
-                <th>email</th>
-                <th>curs</th>
-            </tr>
-        </thead>
-    <tbody>
-
-    @foreach ($professors as $prof)
-        <tr>
-            <td>{{$prof['id']}}</td>
-            <td>{{$prof['nom']}}</td>
-            <td>{{$prof['email']}}</td>
-            <td>{{$prof['curs']}}</td>
-        </tr>
-    @endforeach
-</tbody>
-</table>
+    <!-- Bootstrap JS and Popper.js (required for Bootstrap) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.8/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
